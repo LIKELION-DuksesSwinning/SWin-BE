4b88b4523a425c3850119092ee65a2ce0bb8ef15
seobity <codemin1010@gmail.com>
2026-08-14 20:37:50 +0900

    feat: schedules models.py 주석 해제

--- schedules/models.py
+++ schedules/models.py
@@ -1,24 +1,24 @@
 from django.db import models
 from django.conf import settings
 
-#class Schedule(models.Model):  # 혜원 clinic 모델 앱 구현 후 
-    # CATEGORY_CHOICES = (
-    #     ('SWIM', '수영'),
-    #     ('CLINIC', '클리닉'),
-    # )
-    # user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='schedules')
-    # category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
-    # start_datetime = models.DateTimeField()
-    # end_datetime = models.DateTimeField()
-    # memo = models.TextField(null=True, blank=True)
-    # is_repeat = models.BooleanField(default=False)
-    # repeat_option = models.CharField(max_length=50, null=True, blank=True, help_text="주마다 반복 등")
+class Schedule(models.Model):  # 혜원 clinic 모델 앱 구현 후 
+    CATEGORY_CHOICES = (
+        ('SWIM', '수영'),
+        ('CLINIC', '클리닉'),
+    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='schedules')
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    memo = models.TextField(null=True, blank=True)
+    is_repeat = models.BooleanField(default=False)
+    repeat_option = models.CharField(max_length=50, null=True, blank=True, help_text="주마다 반복 등")
     
-    # # 🦁 clinic 앱과의 연동을 위한 외래키 (Null 허용)
-    # clinic_reservation = models.ForeignKey(
-    #     'clinic.ClinicReservation', 
-    #     on_delete=models.SET_NULL, 
-    #     null=True, 
-    #     blank=True,
-    #     related_name='schedules'
-    # ) 
+    # 🦁 clinic 앱과의 연동을 위한 외래키 (Null 허용)
+    clinic_reservation = models.ForeignKey(
+        'clinic.ClinicReservation', 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        blank=True,
+        related_name='schedules'
+    ) 
