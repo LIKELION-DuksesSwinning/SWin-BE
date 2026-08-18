ca5f620c8f5f5c76aaead7ce3e9c5bf965381845
seobity <codemin1010@gmail.com>
2026-08-18 14:55:29 +0900

    chore: admin 추가

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,11 @@
+# records/admin.py
 from django.contrib import admin
+from .models import SwimRecord, SwimRecordSymptom
 
-# Register your models here.
+@admin.register(SwimRecord)
+class SwimRecordAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'timing', 'created_at')
+
+@admin.register(SwimRecordSymptom)
+class SwimRecordSymptomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'swim_record', 'symptom_type', 'score')
